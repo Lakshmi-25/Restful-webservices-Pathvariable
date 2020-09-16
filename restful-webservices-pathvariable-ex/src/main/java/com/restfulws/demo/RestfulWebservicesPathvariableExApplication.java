dfda6e8bb82b89611bfcef139fec30b138fb8031
Lakshmi-25 <67408945+Lakshmi-25@users.noreply.github.com>
2020-09-16 15:39:37 +0800

    Add files via upload

---
+++ restful-webservices-pathvariable-ex/src/main/java/com/restfulws/demo/RestfulWebservicesPathvariableExApplication.java
@@ -0,0 +1,13 @@
+package com.restfulws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesPathvariableExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesPathvariableExApplication.class, args);
+	}
+
+}
---
+++ restful-webservices-pathvariable-ex/src/test/java/com/restfulws/demo/RestfulWebservicesPathvariableExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulws.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesPathvariableExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
